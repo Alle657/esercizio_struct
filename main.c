dce389c17f54fee8f4da465f15bee4ef8f5482d4
Alle657 <alessandro.tarasco23@gmail.com>
2025-03-12 09:02:54 +0100

    a

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 
-#define MAX_STUDENTI 3
+#define MAX_STUDENTI 2
 typedef struct {
     unsigned  int matricola;
     int eta;
     float media;
+
 } studente;
 
 
 int main(void) {
+    float media_totale = 0;
     studente classe[MAX_STUDENTI];
     for(int i = 0; i< MAX_STUDENTI;i++) {
         printf("inserisci dati dello studente %d\n", i + 1);
@@ -25,11 +27,24 @@ int main(void) {
         printf("inserisci media\n");
         scanf("%f", &classe[i].media);
     } while (classe[i].media < 1.0 || classe[i].media > 10.0);
+    media_totale += classe[i].media;
+
+
 }
+
+
     for(int i = 0; i< MAX_STUDENTI;i++) {
-        printf("studente: %d  matricola %u eta %d media %2.f\n", i+1, classe[i].matricola,classe[i].eta, classe[i].media);
+        printf("studente: %d  matricola: %u eta: %d media: %2.f\n", i+1, classe[i].matricola,classe[i].eta, classe[i].media);
 
     }
+    printf("studenti con media superiore alla media della classe");
+    for(int i = 0; i< MAX_STUDENTI;i++) {
+        if(classe[i].media > media_totale) {
+            printf("studente: %d  matricola: %u eta: %d media: %2.f\n", i+1, classe[i].matricola,classe[i].eta, classe[i].media);
+        }
+
+    }
+
 
 
     return 0;

b267150b198ea0db15fa2c1d327db8d4ab999988
Alle657 <alessandro.tarasco23@gmail.com>
2025-03-12 08:45:43 +0100

    a

--- main.c
+++ main.c
@@ -13,19 +13,23 @@ int main(void) {
     studente classe[MAX_STUDENTI];
     for(int i = 0; i< MAX_STUDENTI;i++) {
         printf("inserisci dati dello studente %d\n", i + 1);
-
         do {
+            printf("inserisci matricola\n");
             scanf("%u", &classe[i].matricola);
         } while (classe[i].matricola < 10000 || classe[i].matricola > 99999);
-
-
-
     do {
+        printf("inserisci eta\n");
         scanf("%d", &classe[i].eta);
-    } while (classe[i].eta);
-
+    } while (classe[i].eta < 13);
+    do {
+        printf("inserisci media\n");
+        scanf("%f", &classe[i].media);
+    } while (classe[i].media < 1.0 || classe[i].media > 10.0);
 }
+    for(int i = 0; i< MAX_STUDENTI;i++) {
+        printf("studente: %d  matricola %u eta %d media %2.f\n", i+1, classe[i].matricola,classe[i].eta, classe[i].media);
 
+    }
 
 
     return 0;
